8c398fa86018f186f47d07345fd2bd0084bfaa69
Rifqie Rusyadi <31301534+rifqierusyadi@users.noreply.github.com>
2017-10-13 09:08:44 +0800

    rest api update satker

--- application/modules/api/controllers/Satker.php
+++ application/modules/api/controllers/Satker.php
@@ -14,14 +14,12 @@ class Satker extends REST_Controller {
     function index_get() {
         $kode = $this->get('kode');
         if ($kode == '') {
-            $this->db->select('unker_id as kode_unker, kode as kode_satker, parent_id as parent, satker');
-            $this->db->where('deleted_at',null);
-            $satker = $this->db->get('ref_satker')->result();
+            $this->db->select('unker_id as kode_unker, kode as kode_satker, parent_id as parent, satker, upt, level, name_level1 as level1, name_level2 as level2, name_level3 as level3,name_level4 as level4, name_level5 as level5, name_level6 as level6, path');
+            $satker = $this->db->get('view_satker')->result();
         } else {
-            $this->db->select('unker_id as kode_unker, kode as kode_satker, parent_id as parent, satker');
+            $this->db->select('unker_id as kode_unker, kode as kode_satker, parent_id as parent, satker, upt, level, name_level1 as level1, name_level2 as level2, name_level3 as level3,name_level4 as level4, name_level5 as level5, name_level6 as level6, path');
             $this->db->where('kode',$kode);
-            $this->db->where('deleted_at',null);
-            $satker = $this->db->get()->result();
+            $satker = $this->db->get('view_satker')->result();
         }
         $this->response($satker, 200);
     }
